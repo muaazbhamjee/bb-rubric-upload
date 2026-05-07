0df44d8063d815fd907a6a01b4420b0a68258934
muaazbhamjee <muaazb@gmail.com>
2026-05-07 12:35:36 +0200

    Updated Email Google Script

--- src/email.js
+++ src/email.js
@@ -1,14 +1,22 @@
 // ─── Configuration ────────────────────────────────────────────────────────
 const FOLDER_ID = "PASTE_YOUR_FOLDER_ID_HERE";
 const STUDENT_EMAIL_DOMAIN = "tuks.co.za";
-const SENDER_NAME = "Dr M. Bhamjee";
-const EMAIL_SUBJECT = "MKM411 Semester Test 1 — Marking Rubric";
+const SENDER_NAME = "Prof M. Bhamjee";
+const EMAIL_SUBJECT = "MKM411 Semester Test 2 — Marking Rubric";
 const EMAIL_BODY =
   "Dear Student,\n\n" +
-  "Please find attached your marking rubric for MKM411 Semester Test 1.\n\n" +
+  "Please find attached your marking rubric for MKM411 Semester Test 2.\n\n" +
   "If you have any queries regarding your marks, please contact me during consultation hours.\n\n" +
+  "Please take note of the below critical information regarding the marking rubric versus the mark and GA5 Competency Assessment on clickUP:\n" +
+  "1. The marks were adjusted up by 8%. The rubric reflects the mark pre-adjustment. The mark on clickUP reflects the adjusted mark.\n" +
+  "2. In terms of assessing GA5 Competency, the rubric provides the original assessment criteria. If you met those criteria which is 4 or greater then you have passed the GA5 component.\n" +
+  "3. We removed the GA5-4 and GA5-7 criteria from the rubric. Thus, the revised criteria requires you to obtain 3 or greater then you have passed the GA5 component.\n\" +
+  "4. The rubric reflects the original GA5 assessment criteria. The ST2 GA5 column in clickUP reflects the adjusted criteria.\n" +
+  "5. On clickUP, the GA5 Competency Assessment is listed as 'ST2 GA5' and is separate from the mark. The mark reflects your overall mark for the test, while the 'ST2 GA5' column indicates whether you passed the GA5 competency based on the adjusted criteria.\n\n" +
+  "6. On clickUP for the ST2 GA5 column, a mark of 1/1 means you are deemed 'Competent' and that means you passed the GA5 component based on the adjusted criteria, while a mark of 0/1 means you are deemed 'Not Yet Competent' which means you did not meet the adjusted criteria for passing the GA5 component.\n\n" +
+  "Please review your rubric and the information above carefully. 
   "Kind regards,\n" +
-  "Dr M. Bhamjee\n" +
+  "Prof M. Bhamjee\n" +
   "Department of Mechanical and Aeronautical Engineering\n" +
   "University of Pretoria";
 
@@ -115,9 +123,10 @@ function createVerificationSheet() {
   const folder = DriveApp.getFolderById(FOLDER_ID);
   const files = folder.getFiles();
 
-  const ss = SpreadsheetApp.create("MKM411_ST1_Email_Verification");
+  const ss = SpreadsheetApp.create("MKM411_ST2_Email_Verification");
   const sheet = ss.getActiveSheet();
   sheet.appendRow(["Username", "Email", "Filename", "File Size (KB)"]);
+  sheet.getRange("A:A").setNumberFormat("@");
 
   while (files.hasNext()) {
     const file = files.next();
